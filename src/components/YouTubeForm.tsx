1a3b51b07ecce6dbade33de7b083a0702c244432
Elias Kirui <eliaskips1@gmail.com>
2023-04-12 11:40:18 +0300

    Managing state

---
+++ src/components/YouTubeForm.tsx
@@ -0,0 +1,24 @@
+import { useForm } from "react-hook-form"
+
+const YouTubeForm = () => {
+    const form = useForm()
+    const { register } = form
+  return (
+    <div>
+      <form>
+        <label htmlFor='username'>Username</label>
+        <input type='text' id='username' {...register('username')}/>
+
+        <label htmlFor='email'>E-mail</label>
+        <input type='email' id='email' {...register('email')} />
+
+        <label>Channel</label>
+        <input type='text' id='channel' {...register('channel')} />
+
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default YouTubeForm
